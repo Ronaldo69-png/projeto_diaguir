f8d5f712ad977249a1a4ce05593b6c354a3c8cd6
Noraldo Mineiro <samuelfsb2009@gmail.com>
2025-08-26 08:29:45 -0300

    Create engarrafada.php

---
+++ engarrafada.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0055)https://www.correiodolago.com.br/clientes/engarrafados/ -->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>olá mundo</title>
+</head>
+
+<body>
+<img src="https://www.correiodolago.com.br/site/up/social/b348318d49c7c89551c332f94ac68cc0.jpg">
+<br>
+</body>
+</html>
+<?php
+if php ronaldo
+?>
